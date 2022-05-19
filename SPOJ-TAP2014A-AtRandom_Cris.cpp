6f6993487caa5ced534ff430212575fee65aca88
jorge <jorge@debian10>
2022-05-19 14:57:24 -0300

    problemas primer encuentro

---
+++ SPOJ-TAP2014A-AtRandom_Cris.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : TAP2014-ProblemaA.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <iostream>
+using namespace std;
+
+int main() {
+	int valorAnterior, valorActual;
+	char paloAnterior, paloActual, mezcla;
+	int cartas,i, t, testCases;
+	scanf("%d", &testCases);
+	//cin>>testCases;
+	for(t=1;t<=testCases;t++){
+		scanf("%d", &cartas);
+		//cin>>cartas;
+		valorAnterior=0; paloAnterior='@';
+		mezcla='B';
+		for(i=1; i<=cartas;i++){
+			scanf("%d %c", &valorActual, &paloActual);
+			//cin>>valorActual>>paloActual;
+			if(valorActual==valorAnterior || paloActual==paloAnterior)
+				mezcla='M';
+			valorAnterior=valorActual;
+			paloAnterior = paloActual;
+		}
+		printf("%c\n", mezcla);
+		//cout<<mezcla<<endl;
+	}
+	return 0;
+}
+
